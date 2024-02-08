fda491f29645d192fe197763f930b06140641088
Adrian Nevarez <124970237+Adrianela1@users.noreply.github.com>
2024-02-08 14:22:43 -0600

    ejercicios corregidos

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // Escribe tu código aquí.
+
+// Funcion para obtener las palabras mas largas que un string nos da
+function bigWords(str, arr) {
+    return arr.filter(word => word.length > str.length);
+}
+
+// Función para imprimir un array de strings dentro de una lista en el HTML
+function printArray(arr) {
+    const ul = document.querySelector('ul');
+    ul.innerHTML = ''; 
+    arr.forEach(word => {
+        const li = document.createElement('li');
+        li.textContent = word;
+        ul.appendChild(li);
+    });
+}
+
+
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca'];
+
+
+const filteredArray = bigWords('bocina', myArray);
+
+
+printArray(filteredArray);
